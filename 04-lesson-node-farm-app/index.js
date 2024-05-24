ba81bfb73ed40a929e065d69a9cba7bd9c311410
burak zafer ozcan <ozcanburakzafer@gmail.com>
2024-05-24 17:25:52 +0300

    04-lesson-node-farm-app completed

--- 04-lesson-node-farm-app/index.js
+++ 04-lesson-node-farm-app/index.js
@@ -2,22 +2,45 @@ const fs = require("fs");
 const http = require("http");
 const url = require("url");
 
+const replaceTemplate = require(`./modules/replaceTemplate.js`);
+
+const tempOverview = fs.readFileSync(
+  `${__dirname}/temps/template-overview.html`,
+  "utf-8"
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/temps/template-card.html`,
+  "utf-8"
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/temps/template-product.html`,
+  "utf-8"
+);
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
-  console.log(req.url);
-  const pathName = req.url;
+  const { query, pathname } = url.parse(req.url, true);
   // overview page
-  if (pathName === "/" || pathName === "/overview") {
-    res.end("hello from the OVERVIEW");
+  if (pathname === "/" || pathname === "/overview") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join("");
+    const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
+    res.end(output);
   }
   // product page
-  else if (pathName === "/product") {
-    res.end("hello from the PRODUCT");
+  else if (pathname === "/product") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
   }
   // API
-  else if (pathName === "/api") {
+  else if (pathname === "/api") {
     fs.readFile(`${__dirname}/dev-data/data.json`, "utf-8", (err, data) => {
       res.writeHead(200, {
         "Content-Type": "application/json",
