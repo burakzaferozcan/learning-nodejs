c56caca1c7dc2f989b4488919db936a1e5f9201b
burak zafer ozcan <ozcanburakzafer@gmail.com>
2024-05-30 17:51:24 +0300

    Connect to the database with mongoose

--- 08-lesson-natours-app/server.js
+++ 08-lesson-natours-app/server.js
@@ -1,6 +1,11 @@
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const app = require("./app.js");
 dotenv.config({ path: "./.env" });
+const DB = process.env.DATABASE;
+mongoose.connect(DB).then(() => {
+  console.log("db connection successfull");
+});
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(`app runing run port ${port}`);
