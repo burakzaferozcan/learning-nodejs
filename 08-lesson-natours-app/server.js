8267873c181f16d47fbb7e078fe7026b17c1b5c7
burak zafer ozcan <ozcanburakzafer@gmail.com>
2024-06-01 22:48:18 +0300

    Creating a Simple Tour Model

--- 08-lesson-natours-app/server.js
+++ 08-lesson-natours-app/server.js
@@ -6,6 +6,22 @@ const DB = process.env.DATABASE;
 mongoose.connect(DB).then(() => {
   console.log("db connection successfull");
 });
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "A tour must have a name"],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, "A tour must have a price"],
+  },
+});
+const Tour = mongoose.model("Tour", tourSchema);
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(`app runing run port ${port}`);
