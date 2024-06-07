1b8f460432e19bd8cb3c60fb3b918ce89b88eb5f
burak zafer ozcan <ozcanburakzafer@gmail.com>
2024-06-07 18:11:18 +0300

    Refactoring for MVC and Another Way of Creating Documents

--- 08-lesson-natours-app/routes/tourRoutes.js
+++ 08-lesson-natours-app/routes/tourRoutes.js
@@ -6,14 +6,7 @@ const {
   createTour,
   updateTour,
   deleteTour,
-  checkID,
-  checkBody,
 } = require("../controllers/tourController.js");
-router.param("id", checkID);
-router.route("/").get(getAllTours).post(checkBody, createTour);
-router
-  .route("/:id")
-  .get(getTour)
-  .patch(checkBody, updateTour)
-  .delete(deleteTour);
+router.route("/").get(getAllTours).post(createTour);
+router.route("/:id").get(getTour).patch(updateTour).delete(deleteTour);
 module.exports = router;
--- 08-lesson-natours-app/server.js
+++ 08-lesson-natours-app/server.js
@@ -6,36 +6,6 @@ const DB = process.env.DATABASE;
 mongoose.connect(DB).then(() => {
   console.log("db connection successfull");
 });
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "A tour must have a name"],
-    unique: true,
-  },
-  rating: {
-    type: Number,
-    default: 4.5,
-  },
-  price: {
-    type: Number,
-    required: [true, "A tour must have a price"],
-  },
-});
-const Tour = mongoose.model("Tour", tourSchema);
-
-const testTour = new Tour({
-  name: "test tour",
-  rating: 4.7,
-  price: 1000,
-});
-testTour
-  .save()
-  .then((doc) => {
-    console.log(doc);
-  })
-  .catch((err) => {
-    console.log("error : ", err);
-  });
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
